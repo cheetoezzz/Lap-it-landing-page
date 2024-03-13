4cb5354d5e4da563769c885f45740014320a02ad
Adrianebucod <Yanyanbucod@gmail.com>
2024-03-13 16:35:15 +0800

    Adriane bucod

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,11 +1,6 @@
-import Hero from "./components/Hero";
-import Features from "./components/Features";
-import FAQs from "./components/FAQs";
+
 import Footer from "./components/footer";
 import Navbar from "./components/navbar";
-import About from "./components/About";
-import Pricing from "./components/Pricing";
-
 const page = () => {
   return (
     <>

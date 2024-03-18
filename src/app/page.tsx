e0fac5f057280a85941a5eceab02974dac9d221a
allenJames7777 <alluvido_200000000403@uic.edu.ph>
2024-03-18 09:46:19 +0800

    Your commit message

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,10 +1,10 @@
 import Hero from "./components/Hero";
 import Features from "./components/Features";
-import FAQs from "./components/FAQs";
+import FAQs from "./components/Faqs";
 import Footer from "./components/footer";
 import Navbar from "./components/navbar";
 import About from "./components/About";
-import Pricing from "./components/Pricing";
+import Pricing from "./Pricing/page";
 
 export default function Home() {
   return (

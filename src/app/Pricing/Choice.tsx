f33a5b2c4b1a07e89d8b7305675f00b6d67636e6
allenJames7777 <alluvido_200000000403@uic.edu.ph>
2024-03-19 11:54:25 +0800

    Global color by allen

---
+++ src/app/Pricing/Choice.tsx
@@ -0,0 +1,13 @@
+import React from 'react'
+
+const Choice = () => {
+  return (
+    <div className='flex justify-center mt-12'>
+      Hello
+      
+      </div>
+    
+  )
+}
+
+export default Choice
--- src/app/Pricing/page.tsx
+++ src/app/Pricing/page.tsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import { Poppins } from 'next/font/google';
 import Navbar from '../components/navbar';
+import Choice from '../Pricing/Choice';
 
 const poppins_init = Poppins({
   subsets: ['latin'],
@@ -8,16 +9,21 @@ const poppins_init = Poppins({
   variable: '--font-Poppins',
 });
 
+
+
 const Pricing = () => {
   return (
     <><Navbar />
-    <div className="flex justify-center">
+    <div className=''>
+    <div className="flex justify-center mt-9">
       <div className={poppins_init.variable}>
         <div className='flex flex-col items-center'>
           <h1 className="text-blue-800 text-4xl poppins font-extrabold">Choose The Perfect Plan For Your Needs</h1>
-          <p className='poppins font-medium text-gray-600'>Free 30-Day Trial For Every Tier!</p>
+          <p className='poppins font-medium text-gray-600 mt-2'>Free 30-Day Trial For Every Tier!</p>
         </div>
+        <Choice/>
       </div>
+    </div>
     </div></>
   );
 };

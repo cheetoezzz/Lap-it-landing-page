fb9936113ebd83b581f3f72dcf112636b46cfe46
Adrianebucod <Yanyanbucod@gmail.com>
2024-04-02 09:24:35 +0800

    NAVBAR RESPONSIVE - BUCOD

--- src/app/components/navbar.tsx
+++ src/app/components/navbar.tsx
@@ -15,17 +15,17 @@ const Navbar = () => {
         </Link>
       </div>
       <div className="flex items-center text-xs ">
-        <ul className="flex items-center space-x-2 sm:space-x-4 md:space-x-6 xl:space-x-8 text-white lg:flex ">
+        <ul className="flex items-center space-x-2 sm:space-x-4 md:space-x-5 xl:space-x-8 text-white lg:flex ">
           {NAV_LINKS.map((link) => (
             <li key={link.key}>
-              <Link href={link.href} className="text-xs sm:text-sm md:text-base font-semibold hover:text-gray-300 duration-300 transition-all">
+              <Link href={link.href} className="text-[8px] sm:text-sm md:text-base font-semibold hover:text-gray-300 duration-300 transition-all">
                 {link.label}
               </Link>
             </li>
           ))}
         </ul>
             <div className="sm:pr-3 md:pr-7 xl:pr-12">
-        <a href=""><button className="ml-3 px-5 py-1 rounded-md sm:text-sm sm:ml-5 md:ml-7 md:text-base  bg-white">Login</button></a>
+        <a href=""><button className="ml-3 px-3 py-1 text-[8px] rounded-md sm:text-sm sm:ml-5 md:ml-7 md:text-base  bg-white">Login</button></a>
         </div>
 
       </div>

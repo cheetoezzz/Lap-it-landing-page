1375fe47fa0787d385089908dd06cd305064ba03
Adrianebucod <Yanyanbucod@gmail.com>
2024-04-05 15:31:51 +0800

    Bucod

--- src/app/components/constants/index.tsx
+++ src/app/components/constants/index.tsx
@@ -2,8 +2,8 @@ import { title } from "process";
 
 export const NAV_LINKS = [
 
-  { href: "/", key: "home", label: "Home" },
-  { href: "/", key: "features", label: "Features" },
+  { href: "/.", key: "home", label: "Home" },
+  { href: "/features", key: "features", label: "Features" },
   { href: "/", key: "FAQs", label: "FAQ" },
   { href: "/", key: "About", label: "About" },
   { href: "/pricing", key: "Pricing", label: "Pricing" },

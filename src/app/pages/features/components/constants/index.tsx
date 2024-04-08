aba42a801d4772d25082810ff6f64f99366b858a
Adrianebucod <Yanyanbucod@gmail.com>
2024-04-08 14:35:47 +0800

    navigation update - bucod

--- src/app/pages/features/components/constants/index.tsx
+++ src/app/pages/features/components/constants/index.tsx
@@ -1,8 +1,8 @@
 export const NAV_LINKS = [
     { href: '/.', key: 'home', label: 'Home' },
     { href: '/pages/features', key: 'features', label: 'Features' },
-    { href: '/', key: 'FAQs', label: 'FAQs' },
+    { href: '/pages/faq', key: 'FAQs', label: 'FAQs' },
     { href: '/pages/about', key: 'About', label: 'About' },
-    { href: '/pricing', key: 'Pricing', label: 'Pricing' },
+    { href: '/pages/pricing', key: 'Pricing', label: 'Pricing' },
     { href: '/', key: 'feedback', label: 'feedback' },
 ];

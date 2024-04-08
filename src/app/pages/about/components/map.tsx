4693520ebb59450075f237bf9ee5f6de04d3ad41
Adrianebucod <Yanyanbucod@gmail.com>
2024-04-08 14:51:39 +0800

    Done

--- src/app/pages/about/components/map.tsx
+++ src/app/pages/about/components/map.tsx
@@ -1,24 +1,33 @@
 'use client'
 
 import 'leaflet/dist/leaflet.css';
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import { MapContainer, TileLayer, Marker, Popup } from 'react-leaflet';
 import L from 'leaflet';
 
-const customIcon = new L.Icon({
-  iconUrl: "/about/location.png",
-  iconSize: [30, 30], // Adjust icon size as needed
-});
-
 const Map = () => {
+  const [customIcon, setCustomIcon] = useState<L.Icon | null>(null);
+
+  useEffect(() => {
+    import('leaflet').then((L) => {
+      const icon = new L.Icon({
+        iconUrl: "/about/location.png",
+        iconSize: [30, 30],
+      });
+      setCustomIcon(icon);
+    });
+  }, []);
+
   return (
-    <MapContainer  center={[7.068593, 125.61187]} zoom={20} style={{ height: '400px', width: '600px', borderRadius: '10px' }}>
+    <MapContainer center={[7.068593, 125.61187]} zoom={20} style={{ height: '400px', width: '600px', borderRadius: '10px' }}>
       <TileLayer url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png" />
-      <Marker position={[7.068593, 125.61187]} icon={customIcon}>
-        <Popup>
-          LAP IT SOLUTIONS INC.
-        </Popup>
-      </Marker>
+      {customIcon && (
+        <Marker position={[7.068593, 125.61187]} icon={customIcon}>
+          <Popup>
+            LAP IT SOLUTIONS INC.
+          </Popup>
+        </Marker>
+      )}
     </MapContainer>
   );
 };

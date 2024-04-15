756b0aae1e8578e8858c6e4f771ab513a7ec660a
ArcTorralba <atorralba_19000000272@uic.edu.ph>
2024-04-15 16:09:24 +0800

    updated contents features and thirdsec

--- src/app/components/card.tsx
+++ src/app/components/card.tsx
@@ -1,25 +1,29 @@
 import React from "react";
+import Image from "next/image";
 
-interface CardProps { //unaha ni tas adto sa features na file // dri ka nga page pod mag design
+interface CardProps {
   description: string;
   title: string;
-  iconSrc?: string;
+  iconSrc: string;
 }
 
-const Card = ({ description, title, iconSrc }: CardProps) => {
+const Card: React.FC<CardProps> = ({ description, title, iconSrc }) => {
   return (
-    <div className="">
-      <div className="w-72 h-72 border bg-white border-blue-500 rounded-md ml-4 shadow-xl flex flex-col justify-center hover:scale-110 duration-700 transition-all">
-        <div className="flex flex-col justify-center items-center">
-          <h1 className="text-black font-semibold text-2xl mt-14">{title}</h1>
-          <div className="text-slate-400 mt-8 px-12">{description}</div>
-          <a
-            href="#"
-            className="border border-blue-500 w-52 h-10 flex justify-center items-center mt-5 rounded-md text-blue-600 font-semibold"
-          >
-            Read more
-          </a>
+    <div className="w-[350px] h-96 border bg-white border-blue-500 rounded-md shadow-xl flex flex-col justify-center hover:scale-110 duration-700 transition-all">
+      <div className="flex flex-col justify-center items-center p-4 h-full">     
+      <Image className="mt-7" src={iconSrc} alt={title} width={50} height={50} />
+        <h1 className="text-black font-semibold text-lg pt-10 text-center">
+          {title}
+        </h1>
+        <div className="text-black text-sm mt-2 mb-8 text-center h-full flex justify-center items-center">
+          {description}
         </div>
+        <a
+          href="#"
+          className="border border-blue-500 w-full h-40 rounded-md text-blue-600 font-semibold flex justify-center items-center mt-auto"
+        >
+          Read more
+        </a>
       </div>
     </div>
   );

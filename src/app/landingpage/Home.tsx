16720858f20d92e55dec489bc85615e9718b2d29
ArcTorralba <atorralba_19000000272@uic.edu.ph>
2024-04-15 14:01:17 +0800

    updated contents

--- src/app/landingpage/Home.tsx
+++ src/app/landingpage/Home.tsx
@@ -2,7 +2,7 @@ import React from "react";
 import Image from "next/image";
 import Card from "../components/constants";
 
-const Home = () => { 
+const Home = () => {
   const card = [
     { title: "100K+", description: "Total Customer" },
     { title: "100K+", description: "Real Time Customer" },
@@ -12,37 +12,42 @@ const Home = () => {
   return (
     <div className="select-none bg-blue-600">
       <div className="max-w-screen-xl mx-auto">
-        <section className="padding-container flex flex-col gap-7 py-5 xl:flex-row">
+        <section className="padding-container flex flex-col gap-20 py-5 xl:flex-row">
           <div className="mt-7">
             <div className="pt-10">
-              <h1 className="text-4xl font-extrabold text-white mb-4">
+              <h1 className="text-5xl font-extrabold text-white mb-4">
                 AFFORDABLE SMALL
               </h1>
-              <h1 className="text-4xl font-extrabold text-white mb-4">
+              <h1 className="text-5xl font-extrabold text-white mb-4">
                 BUSINESS{" "}
                 <span className="bg-white px-1 w-6 text-blue-600 rounded-md">
                   ENTERPRISE
                 </span>
               </h1>
-              <h1 className="text-4xl font-extrabold text-white">SOFTWARE</h1>
-              <p className="text-white mt-7 text-lg">
-                Lorem ipsum dolor sit amet consectetur. Id posuere vulputate diam
-                <br />
-                facilisis dui consectetur. Sed nunc sed nunc ut in diam ultrices
-                <br />
-                blandit. Aenean varius elit sagittis praesent id leo. Turpis
-                mauris
-                <br />
-                accumsan lobortis congue non velit diam massa.
+              <h1 className="text-5xl font-extrabold text-white">SOFTWARE</h1>
+              <p className="text-white mt-7 text-md text-justify mx-auto max-w-lg">
+                Empower your business with cutting-edge tools designed to
+                streamline operations, boost productivity, and maximize growth
+                without breaking the bank. Our software is tailored to meet the
+                unique needs of small businesses, offering a comprehensive suite
+                of features that are easy to use and implement.
               </p>
             </div>
             <div>
-              <button className="ml-1 mt-9 border w-44 h-10 rounded-md border-white text-white font-semibold hover:bg-white hover:text-black">Get Started</button>
-              <button className="ml-28 mt-9 border w-44 h-10 rounded-md border-white text-white font-semibold hover:bg-white hover:text-black">Book a demo</button>
-            </div>  
-            <div className="mt-6 my-5-44 flex flex-row justify-between text-lg"> 
+              <button className="-ml-0 mt-9 border w-44 h-10 rounded-md border-white text-white font-semibold hover:bg-white hover:text-black">
+                Get Started
+              </button>
+              <button className="ml-28 mt-9 border w-44 h-10 rounded-md border-white text-white font-semibold hover:bg-white hover:text-black">
+                Book a demo
+              </button>
+            </div>
+            <div className="mt-6 my-5-44 flex flex-row justify-between text-md">
               {card.map((car, index) => (
-                <Card key={index} title={car.title} description={car.description} />
+                <Card
+                  key={index}
+                  title={car.title}
+                  description={car.description}
+                />
               ))}
             </div>
           </div>

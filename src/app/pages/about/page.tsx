9375afebfd181ba138705288d604631917d7ba53
Adrianebucod <Yanyanbucod@gmail.com>
2024-04-18 11:34:27 +0800

    YAWA

--- src/app/pages/about/page.tsx
+++ src/app/pages/about/page.tsx
@@ -4,17 +4,20 @@ import Navbar from "@/app/components/navbar";
 import Firstsection from "./components/firstsec";
 import Secondsection from "./components/secondsec";
 import Thirdsection from "./components/thirdsec";
-import Footer from '@/app/components/constants/Footer'
-import BottomOfFooter from '@/app/components/constants/BottomOfFooter'
+import Footer from "@/app/components/constants/Footer";
+import BottomOfFooter from "@/app/components/constants/BottomOfFooter";
 
 const Page = () => {
   return (
     <div>
       <Navbar />
+      <div className="text-green-500 sm:text-red-500 md:text-fuchsia-700 lg:text-black xl:text-blue-600">
+        responsive
+      </div>
       <Firstsection />
-      <Secondsection/>
-      <Thirdsection/>
-      <Footer/>
+      <Secondsection />
+      <Thirdsection />
+      <Footer />
     </div>
   );
 };

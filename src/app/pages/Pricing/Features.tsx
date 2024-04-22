6f339594e4729a25a05a06f696ad7b9aee8692b0
Unodbukod <Yanyanbucod@gmail.com>
2024-04-22 15:36:28 +0800

    pota

--- src/app/pages/Pricing/Features.tsx
+++ src/app/pages/Pricing/Features.tsx
@@ -16,32 +16,32 @@ const Features = () => {
     <section className="flex items-center justify-center max-h-none mb-20 select-none">
       <div className="flex flex-col">
         <div className="text-center">
-          <h1 className="text-[48px] font-extrabold">PRICING</h1>
-          <h1 className="text-[16px]">Lorem ipsum dolor sit amet consectetur.</h1>
+          <h1 className="text-[48px] font-extrabold"> PRICING</h1>
         </div>
         <div className="flex flex-row mt-10">
           <div>
-            <div className={`w-[400px] h-[613px] border-md bg-white mx-3 rounded-lg flex flex-col items-start pl-4 pt-5 group border-2 hover:border-blue-500 drop-shadow-2xl`}>
+            <div className={`w-[400px] h-[713px] border-md bg-white mx-3 rounded-lg flex flex-col items-start pl-4 pt-5 group border-2 hover:border-blue-500 drop-shadow-2xl`}>
               <div className="ml-5">
-                <h1 className="text-green-400 text-[14px] ml-6 mt-10">Starter</h1>
-                <h1 className="text-blue-600 text-[12px] ml-6 mt-4">First you can try it for free</h1>
-                <h1 className="text-black text-[48px] ml-6 mt-4">₱ 7,500</h1>
+                <h1 className="text-blue-600 text-[48px] ml-6 mt-4">POS Retail</h1>
+                <h1 className="text-black text-[18px] ml-6 mt-4">Per Quarterly</h1>
+                <h1 className="text-black text-[48px] ml-6 ">₱ 45,000</h1>
                 <h1 className="text-black text-[18px] ml-6 mt-4">Per Month</h1>
+                <h1 className="text-black text-[48px] ml-6 ">₱ 800</h1>
                 <div className="space-y-5 mt-9">
                   <div className="flex space-x-5 "><FcCheckmark style={{ fontSize: "24px" }} />
-                    <h1>Lorem urna suspendisse</h1>
+                    <h1>Point of Sale Software</h1>
                   </div>
                   <div className="flex space-x-5"><FcCheckmark style={{ fontSize: "24px" }} />
-                    <h1>Lorem urna suspendisse</h1>
+                    <h1>Computer Set</h1>
                   </div>
                   <div className="flex space-x-5"><FcCheckmark style={{ fontSize: "24px" }} />
-                    <h1>Lorem urna suspendisse</h1>
+                    <h1>Point of Sale Printer</h1>
                   </div>
                   <div className="flex space-x-5"><FcCheckmark style={{ fontSize: "24px" }} />
-                    <h1>Lorem urna suspendisse</h1>
+                    <h1>Cash Drawer</h1>
                   </div>
                   <div className="flex space-x-5"><FcCheckmark style={{ fontSize: "24px" }} />
-                    <h1>Lorem urna suspendisse</h1>
+                    <h1>Barcode Scanner</h1>
                   </div>
                 </div>
                 <div className="flex items-center justify-center translate-x-[13px] mt-10">
@@ -91,32 +91,33 @@ const Features = () => {
             )}
           </div>
           <div>
-            <div className={`w-[400px] h-[613px] border-md bg-white mx-3 rounded-lg flex flex-col items-start pl-4 pt-5 group border-2 hover:border-blue-500 drop-shadow-2xl`}>
+          <div className={`w-[400px] h-[713px] border-md bg-white mx-3 rounded-lg flex flex-col items-start pl-4 pt-5 group border-2 hover:border-blue-500 drop-shadow-2xl`}>
               <div className="ml-5">
-                <h1 className="text-green-400 text-[14px] ml-6 mt-10">Starter</h1>
-                <h1 className="text-blue-600 text-[12px] ml-6 mt-4">First you can try it for free</h1>
-                <h1 className="text-black text-[48px] ml-6 mt-4">₱ 7,500</h1>
+                <h1 className="text-blue-600 text-[48px] ml-6 mt-4">POS Retail</h1>
+                <h1 className="text-black text-[18px] ml-6 mt-4">Per Quarterly</h1>
+                <h1 className="text-black text-[48px] ml-6 ">₱ 45,000</h1>
                 <h1 className="text-black text-[18px] ml-6 mt-4">Per Month</h1>
+                <h1 className="text-black text-[48px] ml-6 ">₱ 800</h1>
                 <div className="space-y-5 mt-9">
                   <div className="flex space-x-5 "><FcCheckmark style={{ fontSize: "24px" }} />
-                    <h1>Lorem urna suspendisse</h1>
+                    <h1>Point of Sale Software</h1>
                   </div>
                   <div className="flex space-x-5"><FcCheckmark style={{ fontSize: "24px" }} />
-                    <h1>Lorem urna suspendisse</h1>
+                    <h1>Computer Set</h1>
                   </div>
                   <div className="flex space-x-5"><FcCheckmark style={{ fontSize: "24px" }} />
-                    <h1>Lorem urna suspendisse</h1>
+                    <h1>Point of Sale Printer</h1>
                   </div>
                   <div className="flex space-x-5"><FcCheckmark style={{ fontSize: "24px" }} />
-                    <h1>Lorem urna suspendisse</h1>
+                    <h1>Cash Drawer</h1>
                   </div>
                   <div className="flex space-x-5"><FcCheckmark style={{ fontSize: "24px" }} />
-                    <h1>Lorem urna suspendisse</h1>
+                    <h1>Barcode Scanner</h1>
                   </div>
                 </div>
                 <div className="flex items-center justify-center translate-x-[13px] mt-10">
                   <Button
-                    onClick={() => setIsModalOpen2(true)}
+                    onClick={() => setIsModalOpen(true)}
                     className="w-[300px] bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded"
                   >
                     Learn More
@@ -161,32 +162,33 @@ const Features = () => {
             )}
           </div>
           <div>
-            <div className={`w-[400px] h-[613px] border-md bg-white mx-3 rounded-lg flex flex-col items-start pl-4 pt-5 group border-2 hover:border-blue-500 drop-shadow-2xl`}>
+          <div className={`w-[400px] h-[713px] border-md bg-white mx-3 rounded-lg flex flex-col items-start pl-4 pt-5 group border-2 hover:border-blue-500 drop-shadow-2xl`}>
               <div className="ml-5">
-                <h1 className="text-green-400 text-[14px] ml-6 mt-10">Starter</h1>
-                <h1 className="text-blue-600 text-[12px] ml-6 mt-4">First you can try it for free</h1>
-                <h1 className="text-black text-[48px] ml-6 mt-4">₱ 7,500</h1>
+                <h1 className="text-blue-600 text-[48px] ml-6 mt-4">POS Retail</h1>
+                <h1 className="text-black text-[18px] ml-6 mt-4">Per Quarterly</h1>
+                <h1 className="text-black text-[48px] ml-6 ">₱ 45,000</h1>
                 <h1 className="text-black text-[18px] ml-6 mt-4">Per Month</h1>
+                <h1 className="text-black text-[48px] ml-6 ">₱ 800</h1>
                 <div className="space-y-5 mt-9">
                   <div className="flex space-x-5 "><FcCheckmark style={{ fontSize: "24px" }} />
-                    <h1>Lorem urna suspendisse</h1>
+                    <h1>Point of Sale Software</h1>
                   </div>
                   <div className="flex space-x-5"><FcCheckmark style={{ fontSize: "24px" }} />
-                    <h1>Lorem urna suspendisse</h1>
+                    <h1>Computer Set</h1>
                   </div>
                   <div className="flex space-x-5"><FcCheckmark style={{ fontSize: "24px" }} />
-                    <h1>Lorem urna suspendisse</h1>
+                    <h1>Point of Sale Printer</h1>
                   </div>
                   <div className="flex space-x-5"><FcCheckmark style={{ fontSize: "24px" }} />
-                    <h1>Lorem urna suspendisse</h1>
+                    <h1>Cash Drawer</h1>
                   </div>
                   <div className="flex space-x-5"><FcCheckmark style={{ fontSize: "24px" }} />
-                    <h1>Lorem urna suspendisse</h1>
+                    <h1>Barcode Scanner</h1>
                   </div>
                 </div>
                 <div className="flex items-center justify-center translate-x-[13px] mt-10">
                   <Button
-                    onClick={() => setIsModalOpen3(true)}
+                    onClick={() => setIsModalOpen(true)}
                     className="w-[300px] bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded"
                   >
                     Learn More

762cc001eda243253f9ccad810986b5377985135
ArcTorralba <atorralba_19000000272@uic.edu.ph>
2024-04-22 13:04:01 +0800

    responsive home & features

--- src/app/landingpage/Features.tsx
+++ src/app/landingpage/Features.tsx
@@ -1,3 +1,4 @@
+"use client";
 import React from "react";
 import Image from "next/image";
 import Card from "../components/card";
@@ -8,6 +9,7 @@ import {
   CarouselNext,
   CarouselPrevious,
 } from "@/components/ui/carousel";
+import { useMediaQuery } from "@react-hook/media-query";
 
 const Features = () => {
   const card = [
@@ -27,31 +29,33 @@ const Features = () => {
       title: "Multi-Tenancy",
       description:
         "Efficiently manage multiple tenants and properties with our powerful Multi-Tenancy Management System",
-      iconSrc: "/multitenancy.svg", 
+      iconSrc: "/multitenancy.svg",
     },
     {
       title: "Inventory Control",
       description:
         "Optimize your inventory management with our comprehensive Inventory Control System",
-      iconSrc: "/ic.svg", 
+      iconSrc: "/ic.svg",
     },
     {
       title: "Rentals",
       description:
         "Discover seamless rental management with our intuitive software",
-      iconSrc: "/rental.svg", 
+      iconSrc: "/rental.svg",
     },
     {
       title: "Clinic / Patient",
       description:
         "Transform your clinic operations with our all-in-one clinic management software",
-      iconSrc: "/clinic.svg", 
+      iconSrc: "/clinic.svg",
     },
   ];
 
+  const isSmallScreen = useMediaQuery("(max-width: 640px)");
+
   return (
     <div className="flex flex-col items-center pt-20 select-none bg-blue-50">
-      <div className="font-extrabold text-5xl">
+      <div className="font-extrabold text-4xl text-center sm:text-5xl md:text-6xl">
         <h1>
           HIGH -{" "}
           <span className="bg-red-600 text-white p-1 px-4 rounded-md inline-block">
@@ -60,7 +64,7 @@ const Features = () => {
           FEATURES
         </h1>
       </div>
-      <div className="text-center mt-7">
+      <div className="text-center mt-7 mx-3">
         <p>
           Our impactful features are <strong>intricately designed</strong> to
           seamlessly align with your workflow, ensuring a highly effective and
@@ -70,22 +74,41 @@ const Features = () => {
       <div className="mt-12">
         <Image src="/computer.svg" alt="image" width={24} height={24} />
       </div>
-
-      <Carousel>
-        <CarouselContent>
+      
+      {isSmallScreen ? (
+        <div className="px-10">
+        <div className="overflow-auto max-w-96 flex gap-2 mt-10 mb-10">
           {card.map((car, index) => (
-            <CarouselItem className="w-[200px] mt-20 pl-10 mb-14 basis-1/3  " key={index}>
+            <div className="mb-10" key={index}>
               <Card
                 title={car.title}
                 description={car.description}
                 iconSrc={car.iconSrc}
               />
-            </CarouselItem>
+            </div>
           ))}
-        </CarouselContent>
-        <CarouselPrevious />
-        <CarouselNext />
-      </Carousel>
+        </div>
+        </div>
+      ) : (
+        <Carousel className="mx-auto">
+          <CarouselContent>
+            {card.map((car, index) => (
+              <CarouselItem
+                className="w-[90px] mt-20 pl-10 mb-14 sm:basis-1/2 md:w-[100px] md:basis-1/2 lg:w-[150px] lg:basis-1/3 xl:w-[200px] xl:basis-1/3 xl:pl-10 "
+                key={index}
+              >
+                <Card
+                  title={car.title}
+                  description={car.description}
+                  iconSrc={car.iconSrc}
+                />
+              </CarouselItem>
+            ))}
+          </CarouselContent>
+          <CarouselPrevious />
+          <CarouselNext />
+        </Carousel>
+      )}
     </div>
   );
 };
--- src/app/landingpage/Home.tsx
+++ src/app/landingpage/Home.tsx
@@ -5,20 +5,20 @@ const Home = () => {
   return (
     <div className="select-none bg-blue-600">
       <div className="max-w-screen-xl mx-auto">
-        <section className="padding-container flex flex-col gap-20 py-5 xl:flex-row">
-          <div className="pt-2">
-            <div className="pt-[80px]">
-              <h1 className="text-5xl font-extrabold text-white mb-4">
+        <section className="padding-container flex flex-col gap-5 py-5 lg:flex-row xl:flex-row">
+          <div className="pt-2 pb-10">
+            <div className="pt-[20px] max-w-none lg:max-w-none lg:pl-10 lg:pt-10">
+              <h1 className="text-3xl text-center font-extrabold text-white mb-2 sm:text-4xl lg:text-start lg:text-[40px] xl:text-5xl">
                 AFFORDABLE SMALL
               </h1>
-              <h1 className="text-5xl font-extrabold text-white mb-4">
+              <h1 className="text-3xl text-center font-extrabold text-white mb-2 sm:text-4xl lg:text-start lg:text-[40px] xl:text-5xl">
                 BUSINESS{" "}
                 <span className="bg-white px-1 w-6 text-blue-600 rounded-md">
                   ENTERPRISE
                 </span>
               </h1>
-              <h1 className="text-5xl font-extrabold text-white">SOFTWARE</h1>
-              <p className="text-white mt-7 text-md text-justify mx-auto max-w-lg">
+              <h1 className="text-3xl text-center font-extrabold text-white sm:text-4xl lg:text-start lg:text-[40px] xl:text-5xl">SOFTWARE</h1>
+              <p className="text-white mt-7 text-sm text-justify mx-5 max-w-none sm:text-lg lg:mx-0 lg:max-w-[500px]">
                 Empower your business with cutting-edge tools designed to
                 streamline operations, boost productivity, and maximize growth
                 without breaking the bank. Our software is tailored to meet the
@@ -26,17 +26,17 @@ const Home = () => {
                 of features that are easy to use and implement.
               </p>
             </div>
-            <div className="mt-9 flex gap-20">
-              <button className="border w-44 h-10 rounded-md border-white text-white font-semibold hover:bg-white hover:text-black">
+            <div className="mt-9 flex gap-16 justify-center mx-5 lg:justify-start lg:pl-5">
+              <button className="border w-44 h-10 rounded-md border-white text-white font-semibold hover:bg-white hover:text-black md:h-12">
                 Get Started
               </button>
-              <button className="border w-44 h-10 rounded-md border-white text-white font-semibold hover:bg-white hover:text-black">
+              <button className="border w-44 h-10 rounded-md border-white text-white font-semibold hover:bg-white hover:text-black md:h-12">
                 Book a demo
               </button>
             </div>
           </div>
-          <div className="w-1/2 relative pb-10">
-            <div className="relative -top-16 -right-24">
+          <div className="relative pb-10 hidden lg:flex-row lg:max-w-none lg:mr-16 lg:block lg:pt-3 xl:block xl:pl-14 ">
+            <div className="relative -top-16 -right-24 lg:-right-10">
               <Image
                 src="/poss.svg"
                 alt="Point of Sale"
@@ -44,7 +44,7 @@ const Home = () => {
                 height={600}
               />
             </div>
-            <div className="flex gap-10 relative mt-[-160px] justify-center mb-2 -right-20">
+            <div className="flex gap-10 relative mt-[-140px] justify-center mb-5 -right-20 lg:-right-10">
               <Image
                 src="/bir.svg"
                 alt="BIR Logo"

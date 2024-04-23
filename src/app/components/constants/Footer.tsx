47fcb5871e365849fa5453113857bad6b5c555d1
allenJames7777 <alluvido_200000000403@uic.edu.ph>
2024-04-23 13:47:12 +0800

    fixed

--- src/app/components/constants/Footer.tsx
+++ src/app/components/constants/Footer.tsx
@@ -101,8 +101,8 @@ const Footer = () => {
     <footer className="bg-blue-900 text-white mt-auto ">
       
       <div className="max-w-none md:grid md:grid-cols-2 xl:flex xl:flex-row xl:gap-16 xl:justify-center 2xl:gap-52 relative ">
-        <div className="">
-          <div className="flex flex-col items-center mt-5 ">
+        <div className="2xl:mt-4 ">
+          <div className="flex flex-col items-center mt-5  ">
             <Link href="/">
               <div style={{ maxWidth: "100%" }}>
                 <Image
@@ -130,7 +130,7 @@ const Footer = () => {
             </div>
           </div>
         </div>
-        <div className="mt-5 md:mt-7  ">
+        <div className="mt-5 md:mt-7 2xl:mt-10  ">
           <div className="font-semibold text-[17px] flex justify-center md:justify-center md:text-[18px] ">
             Useful Links
           </div>
@@ -147,8 +147,8 @@ const Footer = () => {
 </div>
 
         </div>
-        <div className="flex flex-col justify-center items-center md:mt-7 xl:ml-5 xl:mb-10  ">
-          <div className="font-bold text-[17px] md:text-[18px] mt-5 sm:mt-2">Reach out</div>
+        <div className="flex flex-col justify-center items-center md:mt-7 xl:ml-5 xl:mb-12  ">
+          <div className="font-bold text-[17px] md:text-[18px] mt-5 sm:mt-2 ">Reach out</div>
           <div className="flex items-start mt-1 justify-center  w-72 ">
             <Image 
             src="/location.svg" 
@@ -175,7 +175,7 @@ const Footer = () => {
             <div>+63 905 556 5713</div>
           </div>
         </div>
-        <div className="flex flex-col items-center mt-5 md:mt-7 xl:w-80">
+        <div className="flex flex-col items-center mt-5 md:mt-7 xl:w-80 2xl:mt-10">
           <div className="font-bold text-[17px] md:text-[18px]">Get In Touch</div>
           <div className="flex flex-col text-[12px] mt-1">
             <h1 className="mx-10 text-center text-[12px] md:text-[15px] 2xl:text-[16px]">

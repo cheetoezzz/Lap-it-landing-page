3b74be82ffca8f2ace1b00690888ee4ebda6ecd3
allenJames7777 <alluvido_200000000403@uic.edu.ph>
2024-04-23 16:57:29 +0800

    DONE

--- src/app/components/constants/Footer.tsx
+++ src/app/components/constants/Footer.tsx
@@ -101,7 +101,7 @@ const Footer = () => {
     <footer className="bg-blue-900 text-white mt-auto ">
       
       <div className="max-w-none md:grid md:grid-cols-2 xl:flex xl:flex-row xl:gap-16 xl:justify-center 2xl:gap-52 relative ">
-        <div className="2xl:mt-4 ">
+        <div className="2xl:mt-5">
           <div className="flex flex-col items-center mt-5  ">
             <Link href="/">
               <div style={{ maxWidth: "100%" }}>
@@ -147,6 +147,7 @@ const Footer = () => {
 </div>
 
         </div>
+
         <div className="flex flex-col justify-center items-center md:mt-7 xl:ml-5 xl:mb-12  ">
           <div className="font-bold text-[17px] md:text-[18px] mt-5 sm:mt-2 ">Reach out</div>
           <div className="flex items-start mt-1 justify-center  w-72 ">

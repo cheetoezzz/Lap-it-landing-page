a8bcf52fd2434d6a1e21e096b8fbdca9fd89dd22
Adrianebucod <Yanyanbucod@gmail.com>
2024-04-26 21:19:33 +0800

    Fixing

--- src/app/login/dashboard/admin/editlandingpage/landingpage/Thirdsec.tsx
+++ src/app/login/dashboard/admin/editlandingpage/landingpage/Thirdsec.tsx
@@ -205,8 +205,6 @@ const Thirdsection = () => {
               height={300}
               style={{
                 maxWidth: "100%",
-                height: "auto",
-                maxWidth: "100%",
                 height: "auto"
               }} />
           </div>
